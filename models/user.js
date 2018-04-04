6ddd73026e7e668e51c03ba7ef733d8e525f4518
spacexengineer <hunter.casbeer@gmail.com>
2018-04-03 21:09:26 -0700

    add CRUD delete & update; add uuid4

--- models/user.js
+++ models/user.js
@@ -1,23 +1,48 @@
 const mongoose = require('mongoose');
+const uuid4 = require('uuid/v4');
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        emailAddress: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        firstName: String,
-        lastName: String,
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        index: true
     },
-    { timestamps: true },
-)
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    firstName: String,
+    lastName: String,
+}, {
+    timestamps: true
+}, )
+
+userSchema.statics = {
+
+    createUser(newUser) {
+        newUser.userId = uuid4();
+        return newUser
+            .save()
+            .then(user => {
+                return user.toObject();
+            })
+    }
+}
 
 const User = mongoose.model('User', userSchema, 'users');
+
+// This code removes _id and __v from query results
+if (!userSchema.options.toObject) userSchema.options.toObject = {};
+userSchema.options.toObject.transform = (doc, ret) => {
+    const transformed = ret;
+    delete transformed._id;
+    delete transformed.__v;
+    return transformed;
+};
 
 module.exports = User;
--- routers/users.js
+++ routers/users.js
@@ -1,23 +1,62 @@
 const express = require('express');
-const { User, Company } = require('../models');
+const {
+    User,
+    Company
+} = require('../models');
 const router = express.Router();
 
 router
     .route('/')
     .get((req, res, next) => {
         return User.find().then(users => {
-            return res.render('users/index', {users});
+            // return res.render('users/index', {
+            //     users
+            // });
+            return res.json({
+                users
+            });
         });
     })
     .post((req, res, next) => {
-        console.log(req.body)
-        return User.create(req.body).then(() => {
-            return res.status(201).redirect('/users');
-        });
+
+        // return User.create(req.body).then(() => {
+        //     return res.status(201).redirect('/users');
+        // });
+        // console.log("Here is res.body " + res.body);
+        return User.createUser(new User(req.body))
+            .then(() => {
+                return res.status(201).redirect('/users');
+            });
     });
 
 router.get('/new', (req, res, next) => {
     return res.render('users/new')
 });
+
+router
+    .route('/:userId')
+    .get((req, res, next) => {
+        return User.findOne({
+                _id: `${req.params.userId}`
+            })
+            // add populate when applications and messages added later
+            .then(user => {
+                return res.json({
+                    user
+                })
+            })
+    })
+    .patch((req, res, next) => {
+        return User.findByIdAndUpdate(req.params.userId, req.body)
+            .then(() => {
+                return res.redirect(`/users/${req.params.userId}`);
+            })
+    })
+    .delete((req, res, next) => {
+        return User.findByIdAndRemove(req.params.userId)
+            .then(() => {
+                return res.redirect('/users');
+            })
+    })
 
 module.exports = router;
